d35a824c8c6da512530e0b47eb73d1a14ebe806c
Francisco Lopez <franlopbri@gmail.com>
2021-11-18 11:57:49 -0600

    preparando proyecto

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu, 
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameState currentGameState = GameState.menu;
+
+    public static GameManager sharedInstance;
+
+    private PlayerController controller;
+
+
+   void Awake()
+    {
+        if(sharedInstance ==null)
+        {
+            sharedInstance = this;
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        controller = GameObject.Find("Player").
+            GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit") && currentGameState != GameState.inGame)
+        {
+            StartGame();
+        }
+    }
+
+   public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+    }
+
+    public void GamerOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    private void SetGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.menu)
+        {
+            //TODO: colocar la logica del menu
+        }else if(newGameState == GameState.inGame)
+        {//TODO: hay que preparar la escena para jugar
+            controller.StartGame();
+        }else if(newGameState == GameState.gameOver)
+        {
+            //TODO:preparar eñ juego para el Ga,e Over
+        }
+
+        this.currentGameState = newGameState;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variablesdel movimiento del personaje
+    public float jumpForce = 6f;
+    public float runningSpeed = 2f;
+    
+
+    Rigidbody2D rigidBody;
+    Animator animator;
+    Vector3 startPosition;
+
+    const string STATE_ALIVE = "isAlive"; 
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = this.transform.position;
+    }
+
+    public void StartGame()
+    {
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
+
+        Invoke("RestartPosition", 0.2f);
+    }
+
+    void RestartPosition()
+    {
+        this.transform.position = startPosition;
+        this.rigidBody.velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) 
+        {
+            Jump();
+        }
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
+
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.5f, Color.red);
+    }
+
+     void FixedUpdate()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if (rigidBody.velocity.x < runningSpeed)
+            {
+                rigidBody.velocity = new Vector2(runningSpeed,
+                                                 rigidBody.velocity.y);
+            }
+        }
+        else /// si no estamos dentro de la partida
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        { 
+            if (IsTouchingTheGround())
+            {
+
+                rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            }
+        }
+
+    }
+
+    //nos indica si el personaje esta o no tocando el suelo
+    bool IsTouchingTheGround()
+    {
+        if(Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask))
+        {
+            //programar logica de contacto con el suelo
+            return true;
+        }
+        else {
+            //programar logica de no contacto
+            
+            return false;
+          }
+         
+    }
+
+    public void Die()
+    {
+        this.animator.SetBool(STATE_ALIVE, false);
+        GameManager.sharedInstance.GamerOver();
+    }
+} 
